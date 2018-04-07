043bcf78c90cee30e4fab695f20de06fe8cd8524
Adomous Wright <adomous.wright@uconn.edu>
2018-04-07 04:29:08 -0400

    Modified viewer app

--- stentor/mysite/urls.py
+++ stentor/mysite/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/', home.site.urls),
-    url(r'^viewer/', viewer.site.urls)
+    url(r'^home/', home.urls),
+    url(r'^viewer/', viewer.urls)
 ]
